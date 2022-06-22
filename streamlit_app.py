858d98c2f966005537fc812438ee497d0d97c670
wbuhlman <1403367+wbuhlman@users.noreply.github.com>
2022-06-22 16:19:44 -0400

    Update streamlit_app.py
    Add new headers text

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ import streamlit
 
 streamlit.title("Listado de comida")
 
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
